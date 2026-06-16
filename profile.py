01abc7ec634e2fef0283ce67547433a1dda23c37
Andy Phung <lunavzzc@MacBookPro.attlocal.net>
2026-06-16 16:50:27 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,11 @@
+full_name = "Andy Phung"
+nickname = "Andy"
+age = 14
+used_python_before = False
+hobbies = ["playing games", "basketball", "camping"]
+favorites = {
+    "food": "pasta",
+    "sport": "track",
+    "movie": "bullet train"
+}
+print(full_name, age, hobbies)
